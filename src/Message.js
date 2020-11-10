445bbfb82487ced836aa7a9ca550184cb57b55a0
Nacuta Claudiu <nacutac@gmail.com>
2020-11-10 15:28:04 +0200

    vs 0.01

--- src/Message.js
+++ src/Message.js
@@ -1,75 +1,24 @@
 ///rfce
 
-import { Card, CardContent, Typography } from '@material-ui/core'
-import React, { forwardRef } from 'react'
-import './Message.css';
-import { MessageBox } from 'react-chat-elements'
-import 'react-chat-elements/dist/main.css';
-import { MessageList } from 'react-chat-elements'
-import { ChatItem } from 'react-chat-elements'
-
-
-const Message = forwardRef(({ message, username }, ref) => {
-
-    const isUser = username === message.username;
-    return (
-        <div ref={ref} className={`message ${isUser && 'message_user'} `}>
-            <Card className={isUser ? "message_userCard" : "message_questCard"}>
-                < CardContent >
-                    <Typography
-                        color="white"
-                        variant="h5"
-                        component="h2"
-                    >
-                        {!isUser && `${message.username || 'Unknown user'}: `} {message.message}
-                    </Typography>
-                </CardContent >
-            </Card >
-
-
-            {/* <MessageBox
-                position={'left'}
-                type={'photo'}
-                text={'react.svg'}
-                data={{
-                    uri: 'https://www.robinwieruch.de/static/d85ae32863f6f0202206045e5cf1e4c3/9842e/banner.jpg?w=100px',
-                    status: {
-                        click: false,
-                        loading: 0,
-                    }
-                }} /> */}
-
-            {/* <ChatItem
-                avatar={'https://facebook.github.io/react/img/logo.svg'}
-                alt={'Reactjs'}
-                title={'Facebook'}
-                subtitle={'What are you doing?'}
-                date={new Date()}
-                unread={0} />
-
-            <MessageList
-                className='message-list'
-                lockable={true}
-                toBottomHeight={'100%'}
-                dataSource={[
-                    {
-                        position: 'right',
-                        type: 'text',
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                        date: new Date(),
-                    },
-                    {
-                        position: 'right',
-                        type: 'text',
-                        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit',
-                        date: new Date(),
-                    },
-                ]} /> */}
-        </div>
-    )
-})
-
-export default Message
+// import { Card, CardContent, Typography } from '@material-ui/core'
+// import React, { forwardRef } from 'react'
+// import './Message.css';
+// import 'firebase/auth';
 
 
 
+// const auth = firebase.auth();
+// function Message(props) {
+//     const { message, uid, photoURL } = props.message;
+
+//     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+//     return (<>
+//         <div className={`message ${messageClass}`}>
+//             <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+//             <p>{message}</p>
+//         </div>
+//     </>)
+// }
+
+// export default Message
